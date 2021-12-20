d773afc6d321309604fd40044a452155cd0f7683
willopen004 <75485287+willopen004@users.noreply.github.com>
2021-12-20 14:18:03 +0200

    Add files via upload

---
+++ hw1q2.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#define UPPER_CASE_OFFSET ('A' - 'a')
+#define MAX_ASCII 1000000000 // Maximal value of concatenated ASCIIs in the password
+#define THREE_CHARS_MULTIPLIER 1000
+#define TWO_CHARS_MULTIPLIER 100
+#define FALSE 0
+#define TRUE 1
+
+int main()
+{
+    printf("Welcome to the Password Generator!\n");
+    printf("Please enter your first name (lower-case):\n");
+    char inputChar = 0; // input character, assumed lowercased
+    scanf(" %c", &inputChar);
+    int caseOffset = UPPER_CASE_OFFSET; // "case converter", converts to upper case or doesn't convert (0)
+
+    long ASCII = 0; // contains ASCIIs concatenation
+    int ASCIIMultiplier = 0; // concatenated ASCIIs multiplier, so that the next ASCII can be concatenated by addition
+    int noMoreASCII = FALSE; // 0 if we can keep concatenate ASCIIs, 1 else.
+
+    printf("Your Password: ");
+
+    while (inputChar != '.')
+    {
+        inputChar += caseOffset; // convert to upper-case if needed
+
+        ASCIIMultiplier = (inputChar >= 100) ? (THREE_CHARS_MULTIPLIER) : (TWO_CHARS_MULTIPLIER);
+        // check if concatenating the next ASCII is possible.
+        // note: multiplication isn't used here to avoid overflow.
+        if (ASCII <= (MAX_ASCII - inputChar) / ASCIIMultiplier && noMoreASCII == FALSE)
+        {
+            ASCII *= ASCIIMultiplier;
+            ASCII += inputChar;
+        }
+        else // can't concatenate any more ASCIIs
+        {
+            noMoreASCII = TRUE;
+        }
+
+        printf("%c", inputChar);
+
+        if (caseOffset == 0)
+        {
+            caseOffset = UPPER_CASE_OFFSET;
+        }
+        else // caseOffset is 'A' - 'a'
+        {
+            caseOffset = 0;
+        }
+        scanf("%c", &inputChar);
+    }
+    // check if the first 2 chars of "TheMighty" should be concatenated:
+    if (ASCII <= MAX_ASCII / TWO_CHARS_MULTIPLIER && noMoreASCII == FALSE)
+    {
+        ASCII = ASCII * TWO_CHARS_MULTIPLIER + 'T';
+    }
+    if (ASCII <= MAX_ASCII / THREE_CHARS_MULTIPLIER && noMoreASCII == FALSE)
+    {
+        ASCII = ASCII * THREE_CHARS_MULTIPLIER + 'h';
+    }
+    if (ASCII <= MAX_ASCII / THREE_CHARS_MULTIPLIER && noMoreASCII == FALSE)
+    {
+        ASCII = ASCII * THREE_CHARS_MULTIPLIER + 'e';
+    }
+    printf("TheMighty%ld\n", ASCII);
+
+    return 0;
+}
---
+++ hw1q3.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+#define MESSAGE1 "Hurray!"
+#define MESSAGE2 "You Win!!"
+#define MESSAGE3 "How do I get out of this rhombus?"
+
+#define MESSAGE1_LEN 7
+#define MESSAGE2_LEN 9
+#define MESSAGE3_LEN 33
+
+#define MIN_INDEX 1
+#define MAX_INDEX 3
+
+int main()
+{
+    printf("Please select message to print:\n");
+    printf("Enter 1 for \"Hurray!\"\n");
+    printf("Enter 2 for \"You Win!!\"\n");
+    printf("Enter 3 for \"How do I get out of this rhombus?\"\n");
+    int messageIndex = 0; // input index of chosen message
+    scanf("%d", &messageIndex);
+    while (messageIndex < MIN_INDEX || messageIndex > MAX_INDEX)
+    {
+        printf("Invalid message index, please enter an index between 1 and 3:\n");
+        scanf("%d", &messageIndex);
+    }
+
+    int messageLength = 0; // length of chosen message
+    switch (messageIndex)
+    {
+        case 1:
+            messageLength = MESSAGE1_LEN;
+            break;
+        case 2:
+            messageLength = MESSAGE2_LEN;
+            break;
+        case 3:
+            messageLength = MESSAGE3_LEN;
+            break;
+    }
+
+    printf("Please enter your favorite character:\n");
+    char c = 0; // character used for rhombus edges
+    scanf(" %c", &c);
+
+    printf("Please enter edge length:\n");
+    int edgeLength = 0; // number of characters in an edge
+    scanf("%d", &edgeLength);
+    while (2 * edgeLength - 1 < messageLength + 4)
+    {
+        printf("Invalid edge length, please enter a larger number:\n");
+        scanf("%d", &edgeLength);
+    }
+
+    // Draw rhombus:
+    int row_index = 0; // currently drawn raw index
+    while (row_index < 2 * edgeLength - 1) // every iteration draws a single raw
+    {
+        int col_index = 0; // currently drawn col index
+        // while the column index is in the rhombus:
+        while (col_index <= edgeLength - 1 + row_index &&
+               col_index <= 3 * (edgeLength - 1) - row_index)
+        {
+            // first check if it's time to print the message:
+            if (row_index == edgeLength - 1 &&
+                col_index == (2 * edgeLength - 1 - messageLength) / 2)
+            {
+                switch (messageIndex)
+                {
+                    case 1:
+                        printf(MESSAGE1);
+                        break;
+                    case 2:
+                        printf(MESSAGE2);
+                        break;
+                    case 3:
+                        printf(MESSAGE3);
+                        break;
+                }
+                col_index += messageLength;
+            }
+            // check if this location is an edge of the rhombus:
+            else if (col_index == edgeLength - 1 - row_index ||
+                     col_index == edgeLength - 1 + row_index ||
+                     col_index == (-1) * (edgeLength - 1) + row_index ||
+                     col_index == 3 * (edgeLength - 1) - row_index)
+            {
+                printf("%c", c);
+                col_index++;
+            }
+            else // this location is not an edge and not the message
+            {
+                printf(" ");
+                col_index++;
+            }
+        }
+        printf("\n");
+        row_index++;
+    }
+
+    return 0;
+}
+
